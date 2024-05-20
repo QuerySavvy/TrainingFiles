9f4b10ddb00db7e91903cafe6e1ee47481586488
QuerySavvy <145577861+QuerySavvy@users.noreply.github.com>
2024-05-20 14:52:30 +0200

    Update DataBar.py

--- DataBar.py
+++ DataBar.py
@@ -4,20 +4,22 @@ st.title("Streamlit")
 st.subheader("Whats possible, limitations, and solutions")
 
 page1, page2, page3 = st.tabs(["Page 1", "Page 2", "Page 3"])
+col1, col2, col3 = st.columns(3)
 
 with page1:
   #add a side bar
   sidebarbutton = st.button("Add sidebar")
-  if sidebarbutton == True:
-    st.sidebar.write("Yipeeee")
-    #Show the code
-    code = '''sidebarbutton = st.button("Add sidebar")
+  with col1:
     if sidebarbutton == True:
-    st.sidebar.write("Yipeeee")'''
-    st.code(code, language='python')
-  
-  #add a container bar
-  containerbutton = st.button("Add container")
-  if containerbutton == True:
-    with st.container(border=True):
-      st.write("Yipeeee")
+      st.sidebar.write("Yipeeee")
+      #Show the code
+      code = '''sidebarbutton = st.button("Add sidebar")
+      if sidebarbutton == True:
+      st.sidebar.write("Yipeeee")'''
+      st.code(code, language='python')
+
+    #add a container bar
+    containerbutton = st.button("Add container")
+    if containerbutton == True:
+      with st.container(border=True):
+        st.write("Yipeeee")
