59f6f32db8c24cb4aeca82b50452ed5e9c97d471
QuerySavvy <145577861+QuerySavvy@users.noreply.github.com>
2024-05-20 14:59:34 +0200

    Update DataBar.py

--- DataBar.py
+++ DataBar.py
@@ -7,20 +7,25 @@ page1, page2, page3 = st.tabs(["Page 1", "Page 2", "Page 3"])
 col1, col2, col3 = st.columns(3)
 
 with page1:
-    with col1:
-      st.subheader("layouts")
-      #add a side bar
-      sidebarbutton = st.button("Add sidebar")
-      if sidebarbutton == True:
-        st.sidebar.write("Yipeeee")
-        #Show the code
-        code = '''sidebarbutton = st.button("Add sidebar")
-        if sidebarbutton == True:
-        st.sidebar.write("Yipeeee")'''
-        st.code(code, language='python')
-  
-      #add a container bar
-      containerbutton = st.button("Add container")
-      if containerbutton == True:
-        with st.container(border=True):
-          st.write("Yipeeee")
+    with col1: #Add the buttons
+        st.subheader("layouts")
+        sidebarbutton = st.button("Add sidebar") #-- add a side bar
+        containerbutton = st.button("Add container") #-- add a container
+        
+if sidebarbutton == True:
+st.sidebar.write("Yipeeee")
+#Show the code
+code = '''sidebarbutton = st.button("Add sidebar")
+if sidebarbutton == True:
+st.sidebar.write("Yipeeee")'''
+st.code(code, language='python')
+
+#add a container bar
+if containerbutton == True:
+with st.container(border=True):
+    st.write("Yipeeee")
+    #Show the code
+    code = '''if containerbutton == True:
+with st.container(border=True):
+    st.write("Yipeeee")'''
+    st.code(code, language='python')
