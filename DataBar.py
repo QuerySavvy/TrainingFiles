27b39cd7fc596a0d2391d46683c50b3e96ddb923
QuerySavvy <145577861+QuerySavvy@users.noreply.github.com>
2024-05-20 14:00:58 +0200

    Update DataBar.py

--- DataBar.py
+++ DataBar.py
@@ -2,3 +2,8 @@ import streamlit as st
 
 st.title("Streamlit")
 st.subheader("Whats possible, limitations, and solutions")
+
+#add a side bar
+sidebarbutton = st.buttton("Add sidebar")
+if sidebarbutton = True
+  st.sidebar.write("Yipeeee")
