f132aa3fd5734b75f8552231087c340497ebdce8
QuerySavvy <145577861+QuerySavvy@users.noreply.github.com>
2024-05-20 15:25:36 +0200

    Update DataBar.py

--- DataBar.py
+++ DataBar.py
@@ -3,8 +3,16 @@ import streamlit as st
 st.title("Open Data Bar - Streamlit Demo")
 st.subheader("Whats possible, limitations, and solutions")
 
+
+#Page setup
 page1, page2, page3 = st.tabs(["Page 1", "Page 2", "Page 3"])
 col1, col2, col3 = st.columns(3)
+
+code = '''#Page setup
+page1, page2, page3 = st.tabs(["Page 1", "Page 2", "Page 3"])
+col1, col2, col3 = st.columns(3)'''
+        st.code(code, language='python')
+
 
 with page1:
     with col1: #Add the buttons
