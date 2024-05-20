703d6b47eadbaf69aac9bab32b3551645150009e
QuerySavvy <145577861+QuerySavvy@users.noreply.github.com>
2024-05-20 21:50:46 +0200

    Update DataBar.py

--- DataBar.py
+++ DataBar.py
@@ -115,9 +115,9 @@ ORDER BY salary DESC'''
 
     if texttext == True:
         st.divider()
-        st.text("Merci - Nous avons reçu votre demande." 
+        st.text("Merci - Nous avons reçu votre demande.") 
         st.text("Nous vous répondrons dans un délai de 12 mois (minimum).")
-        st.text("Cordialement"
+        st.text("Cordialement")
         st.text("L'administration Française")
         
     with col3: #Add the buttons
