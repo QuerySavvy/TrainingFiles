fabb619b28fadb8eef2cfea3c89b804175c91164
QuerySavvy <145577861+QuerySavvy@users.noreply.github.com>
2024-05-21 11:01:20 +0200

    Update DataBar.py

--- DataBar.py
+++ DataBar.py
@@ -178,7 +178,9 @@ with page2:
 
     #Initialise my session states
     if 'disp_session_state' not in session_state:
-        session_state['disp_session_state'] = False  
+        session_state['disp_session_state'] = False
+    if 'taco' not in session_state:
+        session_state['taco'] = None
     
     st.write("Page2Selected")
     st.write("Launched in 2019. Limited information available besides the official docs. ChatGPT-3.5 goes up to Jan 2022 so isnt very helpful.")
