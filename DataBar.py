85858beef6753f87a8a4f67435d15b22e3f94e4b
QuerySavvy <145577861+QuerySavvy@users.noreply.github.com>
2024-05-21 08:04:18 +0200

    Update DataBar.py

--- DataBar.py
+++ DataBar.py
@@ -188,16 +188,19 @@ with page2:
                 meat = st.multiselect("Choose the meat",["Poulet panné", "Cordon bleu", "kebab"])
             with col_3:
                 sauce = st.multiselect("Choose the sauce",["Allondoise", "Algerienne", "Blanche"])
+                
+            if size and meat and sauce:
+                st.write("Order:" + size + meat + " with" + sauce + "sauce")
 
             code = '''taco = st.button("Make a Taco")
-    if taco:
-        col_1, col_2, col_3 = st.columns(3)
-        with col_1:
-            st.radio("Choose a size",["mini", "une viande", "deux viandes"])
-        with col_2:
-            st.multiselect("Choose the meat",["Poulet panné", "Cordon bleu", "kebab"])
-        with col_3:
-            st.multiselect("Choose the sauce",["Allondoise", "Algerienne", "Blanche"])'''
+if taco:
+    col_1, col_2, col_3 = st.columns(3)
+    with col_1:
+        st.radio("Choose a size",["mini", "une viande", "deux viandes"])
+    with col_2:
+        st.multiselect("Choose the meat",["Poulet panné", "Cordon bleu", "kebab"])
+    with col_3:
+        st.multiselect("Choose the sauce",["Allondoise", "Algerienne", "Blanche"])'''
             st.code(code, language='python')
     
     st.write("session state")
