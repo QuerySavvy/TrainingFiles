fb217f982cbe12ce58b76e3a8b9be6558ea7b5e5
QuerySavvy <145577861+QuerySavvy@users.noreply.github.com>
2024-05-21 10:36:52 +0200

    Update DataBar.py

--- DataBar.py
+++ DataBar.py
@@ -190,6 +190,8 @@ with page2:
             taco3 = st.button("Make a Taco v3")
             
         if taco1:
+            if 'taco' in session_state:
+                session_state['taco'] = None
             col_1, col_2, col_3 = st.columns(3)
             with col_1:
                 size = st.radio("Choose a size",["mini", "une viande", "deux viandes"])
@@ -217,6 +219,8 @@ if taco:
 #-------- Taco v2
                
         if taco2:
+            if 'taco' in session_state:
+                session_state['taco'] = None
             code = '''taco = st.button("Make a Taco")
 if taco:
     session_state['taco'] = True
