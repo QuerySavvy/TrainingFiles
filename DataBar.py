5e1fc829d65dfe0003606c7a1a98adb8818006eb
QuerySavvy <145577861+QuerySavvy@users.noreply.github.com>
2024-05-21 14:49:19 +0200

    Update DataBar.py

--- DataBar.py
+++ DataBar.py
@@ -359,7 +359,7 @@ if session_state['make_taco_v3'] == True:
             session_state['order_taco_v1'] = True
             
         if session_state['order_taco_v1'] == True:
-            user = st.text_input("What is your name")
+            user = st.text_input("What is your name",key="order1")
             if user:
                 with st.spinner("Running big_function"):
                     big_function(user)
@@ -412,7 +412,7 @@ if session_state['order_taco_v1'] == True:
             session_state['order_taco_v2'] = True
             
         if session_state['order_taco_v2'] == True:
-            user = st.text_input("What is your name")
+            user = st.text_input("What is your name",key="order2")
             if user:
                 if session_state['user'] != user:
                     with st.spinner("Running big_function_1"):
